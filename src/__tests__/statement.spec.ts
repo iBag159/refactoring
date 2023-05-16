ca33f34961b58e3999fd155cf5038aff01457911
Gaby Alvarez <galvarezc@unitru.edu.pe>
2023-05-16 13:42:24 -0500

    Chapter1 - rename the variable to get it CLEARER

--- src/__tests__/statement.spec.ts
+++ src/__tests__/statement.spec.ts
@@ -5,6 +5,6 @@ import invoices from '../data/invoices.json';
 describe("Statement", () => {
   it("should returns", () => {
     const result = statement(invoices[0], plays);
-    expect(result).toBe(`Statement for BigCo\nHamlet: $650.00 (55 seats)\nAs You Like It: $580.00 (35 seats)\nOthello: $500.00 (40 seats)\nAmount owed is $1,730.00\nYou earned 47 credits\n`)
+    expect(result).toBe(`Statement for BigCo\nHamlet: $650.00 (55 seats)\nAs You Like It: $580.00 (35 seats)\nOthello: $500.00 (40 seats)\nAmount owed is $1,730.00\nYou earned 47 credits\n`);
   })
 })
