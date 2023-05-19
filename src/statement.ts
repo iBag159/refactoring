45fb619fbd9e82472fdbfbf1ded208644a7ed55a
Gaby Alvarez <galvarezc@unitru.edu.pe>
2023-05-19 16:48:50 -0500

    Chapter1 - Slide Statements. Easy to understand..

--- src/statement.ts
+++ src/statement.ts
@@ -1,14 +1,12 @@
 export function statement (invoice: any, plays: any) {
   let totalAmount = 0;
-  let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
-
   for (let perf of invoice.performances) {
     // print line for this order
     result += `${playFor(perf).name}: ${usd(amountFor(perf)/100)} (${perf.audience} seats)\n`;
     totalAmount += amountFor(perf);
   }
-
+  let volumeCredits = 0;
   for (let perf of invoice.performances) {
     // add volume credits
     volumeCredits += volumeCreditsFor(perf);
