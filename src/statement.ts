00347f714220a9b1b42a3c4cb6a25058d80863e0
Gaby Alvarez <galvarezc@unitru.edu.pe>
2023-05-19 17:40:23 -0500

    Prettier !!!!

--- src/statement.ts
+++ src/statement.ts
@@ -1,38 +1,38 @@
-export function statement (invoice: any, plays: any) {
-  let totalAmount = 0;
-  let result = `Statement for ${invoice.customer}\n`;
+export function statement(invoice: any, plays: any) {
+  let totalAmount = 0
+  let result = `Statement for ${invoice.customer}\n`
   for (let perf of invoice.performances) {
     // print line for this order
-    result += `${playFor(perf).name}: ${usd(amountFor(perf)/100)} (${perf.audience} seats)\n`;
-    totalAmount += amountFor(perf);
+    result += `${playFor(perf).name}: ${usd(amountFor(perf) / 100)} (${perf.audience} seats)\n`
+    totalAmount += amountFor(perf)
   }
-  let volumeCredits = totalVolumeCredits();
+  let volumeCredits = totalVolumeCredits()
 
-  result += `Amount owed is ${usd(totalAmount/100)}\n`;
-  result += `You earned ${volumeCredits} credits\n`;
-  return result;
+  result += `Amount owed is ${usd(totalAmount / 100)}\n`
+  result += `You earned ${volumeCredits} credits\n`
+  return result
 
   //Extract Function (106)
-  function amountFor (aPerformance: any) {
-    let result = 0;
-    switch(playFor(aPerformance).type) {
-      case "tragedy":
-        result = 40000;
-        if(aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30);
+  function amountFor(aPerformance: any) {
+    let result = 0
+    switch (playFor(aPerformance).type) {
+      case 'tragedy':
+        result = 40000
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30)
         }
-        break;
-      case "comedy":
-        result = 30000;
-        if(aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20);
+        break
+      case 'comedy':
+        result = 30000
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20)
         }
-        result += 300 * aPerformance.audience;
-        break;
+        result += 300 * aPerformance.audience
+        break
       default:
-        throw new Error(`Unknown type: ${playFor(aPerformance).type}`);
+        throw new Error(`Unknown type: ${playFor(aPerformance).type}`)
     }
-    return result;
+    return result
   }
 
   //Replace Temp with Query (178)
@@ -41,22 +41,22 @@ export function statement (invoice: any, plays: any) {
   }
 
   function volumeCreditsFor(aPerformance: any) {
-    let result = 0;
+    let result = 0
     result += Math.max(aPerformance.audience - 30, 0)
-    if ("comedy" === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5);
-    return result;
+    if ('comedy' === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5)
+    return result
   }
 
   function usd(aNumber: number) {
-    return new Intl.NumberFormat("en-US", {style: "currency", currency: "USD", minimumFractionDigits: 2}).format(aNumber);
+    return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD', minimumFractionDigits: 2 }).format(aNumber)
   }
 
   function totalVolumeCredits() {
-    let volumeCredits = 0;
+    let volumeCredits = 0
     for (let perf of invoice.performances) {
       // add volume credits
-      volumeCredits += volumeCreditsFor(perf);
+      volumeCredits += volumeCreditsFor(perf)
     }
-    return volumeCredits;
+    return volumeCredits
   }
 }
