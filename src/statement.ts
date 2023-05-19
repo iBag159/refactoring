7b63fec593084f877661adfb1046052b1fea608c
Gaby Alvarez <galvarezc@unitru.edu.pe>
2023-05-19 16:53:28 -0500

    Chapter1 - Extract Function totalVolumeCredis

--- src/statement.ts
+++ src/statement.ts
@@ -6,11 +6,7 @@ export function statement (invoice: any, plays: any) {
     result += `${playFor(perf).name}: ${usd(amountFor(perf)/100)} (${perf.audience} seats)\n`;
     totalAmount += amountFor(perf);
   }
-  let volumeCredits = 0;
-  for (let perf of invoice.performances) {
-    // add volume credits
-    volumeCredits += volumeCreditsFor(perf);
-  }
+  let volumeCredits = totalVolumeCredits();
 
   result += `Amount owed is ${usd(totalAmount/100)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
@@ -53,5 +49,14 @@ export function statement (invoice: any, plays: any) {
 
   function usd(aNumber: number) {
     return new Intl.NumberFormat("en-US", {style: "currency", currency: "USD", minimumFractionDigits: 2}).format(aNumber);
+  }
+
+  function totalVolumeCredits() {
+    let volumeCredits = 0;
+    for (let perf of invoice.performances) {
+      // add volume credits
+      volumeCredits += volumeCreditsFor(perf);
+    }
+    return volumeCredits;
   }
 }
