36da4e31b3c7a3735cc7824659061cc411470ba5
Gaby Alvarez <galvarezc@unitru.edu.pe>
2023-05-19 21:18:08 -0500

    Chapter1 - Inline variable totalVolumeCredits

--- src/statement.ts
+++ src/statement.ts
@@ -6,10 +6,9 @@ export function statement(invoice: any, plays: any) {
     result += `${playFor(perf).name}: ${usd(amountFor(perf) / 100)} (${perf.audience} seats)\n`
     totalAmount += amountFor(perf)
   }
-  let volumeCredits = totalVolumeCredits()
 
   result += `Amount owed is ${usd(totalAmount / 100)}\n`
-  result += `You earned ${volumeCredits} credits\n`
+  result += `You earned ${totalVolumeCredits()} credits\n`
   return result
 
   //Extract Function (106)
@@ -52,11 +51,11 @@ export function statement(invoice: any, plays: any) {
   }
 
   function totalVolumeCredits() {
-    let volumeCredits = 0
+    let result = 0
     for (let perf of invoice.performances) {
       // add volume credits
-      volumeCredits += volumeCreditsFor(perf)
+      result += volumeCreditsFor(perf)
     }
-    return volumeCredits
+    return result
   }
 }
