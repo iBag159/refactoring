5619d968358eaf5cd1caee38b3aa442971aa581f
Gaby Alvarez <galvarezc@unitru.edu.pe>
2023-05-20 16:05:48 -0500

    Chapter1 - Replace the references to playFor

--- src/statement.ts
+++ src/statement.ts
@@ -4,19 +4,23 @@ export function statement(invoice: any, plays: any) {
     performances: invoice.performances.map(enrichPerformance),
   }
   console.log(statementData);
-  return renderPlainText(statementData, plays);
+  return renderPlainText(statementData);
 
   function enrichPerformance(aPerformance: any) {
     const result = Object.assign({}, aPerformance);
+    result.play = playFor(result);
     return result;
+  }
+  function playFor(aPerformance: any) {
+    return plays[aPerformance.playID]
   }
 }
 
-function renderPlainText(data: any, plays: any) {
+function renderPlainText(data: any) {
   let result = `Statement for ${data.customer}\n`
   for (let perf of data.performances) {
     // print line for this order
-    result += `${playFor(perf).name}: ${usd(amountFor(perf) / 100)} (${perf.audience} seats)\n`
+    result += `${perf.play.name}: ${usd(amountFor(perf) / 100)} (${perf.audience} seats)\n`
   }
   result += `Amount owed is ${usd(totalAmount() / 100)}\n`
   result += `You earned ${totalVolumeCredits()} credits\n`
@@ -43,15 +47,12 @@ function renderPlainText(data: any, plays: any) {
   function volumeCreditsFor(aPerformance: any) {
     let result = 0
     result += Math.max(aPerformance.audience - 30, 0)
-    if ('comedy' === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5)
+    if ('comedy' === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5)
     return result
-  }
-  function playFor(aPerformance: any) {
-    return plays[aPerformance.playID]
   }
   function amountFor(aPerformance: any) {
     let result = 0
-    switch (playFor(aPerformance).type) {
+    switch (aPerformance.play.type) {
       case 'tragedy':
         result = 40000
         if (aPerformance.audience > 30) {
@@ -66,7 +67,7 @@ function renderPlainText(data: any, plays: any) {
         result += 300 * aPerformance.audience
         break
       default:
-        throw new Error(`Unknown type: ${playFor(aPerformance).type}`)
+        throw new Error(`Unknown type: ${aPerformance.play.type}`)
     }
     return result
   }
