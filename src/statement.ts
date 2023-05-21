acf43b68bd9e69c407b33682d729381cb182946d
Gaby Alvarez <galvarezc@unitru.edu.pe>
2023-05-20 20:45:54 -0500

    Chapter1 - Html version

--- src/statement.ts
+++ src/statement.ts
@@ -12,6 +12,23 @@ function renderPlainText(data: any) {
   result += `You earned ${data.totalVolumeCredits} credits\n`
   return result
 }
+export function htmlStatement(invoice: any, plays: any) {
+  return renderHtml(createStatementData(invoice, plays));
+}
+function renderHtml(data: any) {
+  let result = `<h1>Statement for ${data.customer}</h1>\n`;
+  result = `<table>\n`;
+  result = `<tr><th>play</th><th>seats</th><th>cost</th></tr>\n`;
+  for (let perf of data.performances) {
+    // print line for this order
+    result += `<tr><td>${perf.play.name}</td><td>${perf.audience}</td>`
+    result += `<td>${usd(perf.amount)}</td></tr>\n`
+  }
+  result += `</table>\n`
+  result += `Amount owed is ${usd(data.totalAmount)}\n`
+  result += `You earned ${data.totalVolumeCredits} credits\n`
+  return result
+}
 function usd(aNumber: number) {
   return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD', minimumFractionDigits: 2 }).format(aNumber/100)
 }
