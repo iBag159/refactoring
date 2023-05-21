d644d893726d017a0a9675b6e8ee2e9ba8658654
Gaby Alvarez <galvarezc@unitru.edu.pe>
2023-05-20 22:26:37 -0500

    Chapter1 - Moving functions in the Calculator

--- src/createStatementData.ts
+++ src/createStatementData.ts
@@ -1,3 +1,33 @@
+class PerformanceCalculator {
+  play: any;
+  performance: any;
+
+  constructor(aPerformance: any, aPlay: any) {
+    this.performance = aPerformance;
+    this.play = aPlay;
+  }
+  get amount() {
+    let result = 0
+    switch (this.play.type) {
+      case 'tragedy':
+        result = 40000
+        if (this.performance.audience > 30) {
+          result += 1000 * (this.performance.audience - 30)
+        }
+        break
+      case 'comedy':
+        result = 30000
+        if (this.performance.audience > 20) {
+          result += 10000 + 500 * (this.performance.audience - 20)
+        }
+        result += 300 * this.performance.audience
+        break
+      default:
+        throw new Error(`Unknown type: ${this.play.type}`)
+    }
+    return result
+  }
+}
 
 export function createStatementData(invoice: any, plays: any) {
   const statementData = {
@@ -11,12 +41,14 @@ export function createStatementData(invoice: any, plays: any) {
   return statementData;
 
   function enrichPerformance(aPerformance: any) {
+    const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
     const result = Object.assign({}, aPerformance)
-    result.play = playFor(result)
+    result.play = calculator.play;
     result.amount = amountFor(result)
     result.volumeCredits = volumeCreditsFor(result)
     return result
   }
+
   function playFor(aPerformance: any) {
     return plays[aPerformance.playID]
   }
