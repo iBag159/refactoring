3ca780916d2e42094b17d4c628a01cd42a3c66a5
Gaby Alvarez <galvarezc@unitru.edu.pe>
2023-05-20 22:31:15 -0500

    Chapter1 - Refactor amountFor into a delegating function

--- src/createStatementData.ts
+++ src/createStatementData.ts
@@ -53,25 +53,7 @@ export function createStatementData(invoice: any, plays: any) {
     return plays[aPerformance.playID]
   }
   function amountFor(aPerformance: any) {
-    let result = 0
-    switch (aPerformance.play.type) {
-      case 'tragedy':
-        result = 40000
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30)
-        }
-        break
-      case 'comedy':
-        result = 30000
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20)
-        }
-        result += 300 * aPerformance.audience
-        break
-      default:
-        throw new Error(`Unknown type: ${aPerformance.play.type}`)
-    }
-    return result
+    return new PerformanceCalculator(aPerformance, playFor(aPerformance)).amount;
   }
   function volumeCreditsFor(aPerformance: any) {
     let result = 0
