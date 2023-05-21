75884939d7268814571280df9937eed8a1439f2f
Gaby Alvarez <galvarezc@unitru.edu.pe>
2023-05-20 20:36:27 -0500

    Fix my incredible mistake.. when usd Function was updated?

--- src/statement.ts
+++ src/statement.ts
@@ -6,13 +6,12 @@ export function statement(invoice: any, plays: any) {
 function renderPlainText(data: any) {
   let result = `Statement for ${data.customer}\n`
   for (let perf of data.performances) {
-    // print line for this order
-    result += `${perf.play.name}: ${usd(perf.amount / 100)} (${perf.audience} seats)\n`
+    result += `${perf.play.name}: ${usd(perf.amount)} (${perf.audience} seats)\n`
   }
-  result += `Amount owed is ${usd(data.totalAmount / 100)}\n`
+  result += `Amount owed is ${usd(data.totalAmount)}\n`
   result += `You earned ${data.totalVolumeCredits} credits\n`
   return result
 }
 function usd(aNumber: number) {
-  return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD', minimumFractionDigits: 2 }).format(aNumber)
+  return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD', minimumFractionDigits: 2 }).format(aNumber/100)
 }
