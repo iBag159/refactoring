b0b19a34eb49ff1f56678431b6a985019bd6ee4c
Gaby Alvarez <galvarezc@unitru.edu.pe>
2023-05-20 23:20:07 -0500

    Chapter1 - Replace the volumeCredits calculation

--- src/createStatementData.ts
+++ src/createStatementData.ts
@@ -6,20 +6,17 @@ class PerformanceCalculator {
     this.performance = aPerformance;
     this.play = aPlay;
   }
-  get amount() {
+  get amount () {
     throw new Error("Subclass responsibility");
     return 0; // typescript things...
   }
-  get volumeCredits() {
-    let result = 0
-    result += Math.max(this.performance.audience - 30, 0)
-    if ('comedy' === this.play.type) result += Math.floor(this.performance.audience / 5)
-    return result
+  get volumeCredits () {
+    return Math.max(this.performance.audience - 30, 0)
   }
 }
 
 class TragedyCalculator extends PerformanceCalculator {
-  get amount() {
+  get amount () {
     let result = 40000
     if (this.performance.audience > 30) {
       result += 1000 * (this.performance.audience - 30)
@@ -29,7 +26,7 @@ class TragedyCalculator extends PerformanceCalculator {
 }
 
 class ComedyCalculator extends PerformanceCalculator {
-  get amount() {
+  get amount () {
     let result = 30000;
     if (this.performance.audience > 20) {
       result += 10000 + 500 * (this.performance.audience - 20)
@@ -37,18 +34,21 @@ class ComedyCalculator extends PerformanceCalculator {
     result += 300 * this.performance.audience;
     return result;
   }
-}
-
-function createPerformanceCalculator(aPerformance: any, aPlay: any) {
-  switch(aPlay.type) {
-    case "tragedy": return new TragedyCalculator(aPerformance, aPlay);
-    case "comedy": return new ComedyCalculator(aPerformance, aPlay);
-    default:
-        throw new Error(`Unknown type: ${aPlay.type}`)
+  get volumeCredits () {
+    return super.volumeCredits + Math.floor(this.performance.audience / 5);
   }
 }
 
-export function createStatementData(invoice: any, plays: any) {
+function createPerformanceCalculator (aPerformance: any, aPlay: any) {
+  switch (aPlay.type) {
+    case "tragedy": return new TragedyCalculator(aPerformance, aPlay);
+    case "comedy": return new ComedyCalculator(aPerformance, aPlay);
+    default:
+      throw new Error(`Unknown type: ${aPlay.type}`)
+  }
+}
+
+export function createStatementData (invoice: any, plays: any) {
   const statementData = {
     customer: invoice.customer,
     performances: invoice.performances.map(enrichPerformance),
@@ -59,7 +59,7 @@ export function createStatementData(invoice: any, plays: any) {
   statementData.totalVolumeCredits = totalVolumeCredits(statementData)
   return statementData;
 
-  function enrichPerformance(aPerformance: any) {
+  function enrichPerformance (aPerformance: any) {
     const calculator = createPerformanceCalculator(aPerformance, playFor(aPerformance));
     const result = Object.assign({}, aPerformance)
     result.play = calculator.play;
@@ -68,14 +68,14 @@ export function createStatementData(invoice: any, plays: any) {
     return result
   }
 
-  function playFor(aPerformance: any) {
+  function playFor (aPerformance: any) {
     return plays[aPerformance.playID]
   }
-  function totalAmount(data: any) {
+  function totalAmount (data: any) {
     return data.performances.reduce((total: any, p: any) => total + p.amount, 0);
   }
 
-  function totalVolumeCredits(data: any) {
+  function totalVolumeCredits (data: any) {
     return data.performances.reduce((total: any, p: any) => total + p.volumeCredits, 0);
   }
 }
